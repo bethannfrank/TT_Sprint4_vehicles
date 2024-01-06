bf3c6d8462a4b8861d7732b6c4e296c75655b809
Elizabeth Frank <bethannfrank@gmail.com>
2024-01-05 23:20:57 -0600

    initial app.py file to test render

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import pandas as pd
-import plotly_express as px
+import plotly.express as px
 
-veh_df = pd.read_csv('vehicles_us.csv')
+veh_df = pd.read_csv('~/Documents/Data Science/Projects/TT_Sprint_4/TT_Sprint4_vehicles/vehicles_us.csv')
+
+#full path '~/Documents/Data Science/Projects/TT_Sprint_4/TT_Sprint4_vehicles/vehicles_us.csv'
 
 print(veh_df.head())
+
+st.header('Histogram of mileage vs condition')
+
+fig_d = px.histogram(veh_df, x='odometer', color='condition', nbins=50)
+st.write(fig_d)
