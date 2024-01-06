9bc0a840babdbad8f858350f75044a48e2b0c774
Elizabeth Frank <bethannfrank@gmail.com>
2024-01-06 10:35:02 -0600

    updated Jupyter, app.py, readme files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # TT_Sprint4_vehicles
-This is a repository for TT Spring 4 Project analyzing car advertisement data.
+This is a repository for TT Spring 4 Project analyzing car advertisement data. It creates a web application on Render to expore the data through visual graphs for random events, i.e., used vehicle sales. Specifically, the graphs look at overall vehicle sales and certain factors (e.g., condition, model year, color) as they compare to price and days listed/days advertised.
+
+This project uses car advertisement data from the US (vehicles_us.csv) available here: https://practicum-content.s3.us-west-1.amazonaws.com/datasets/vehicles_us.csv
+
+To run the program, Python 3 (3.11.5) was used with the following packages: pandas, streamlit, plotly.express.
+
+The virtual environment contained the following versions which were used for the requirements file when deploying to Render:
+pandas==2.1.4
+streamlit==1.29.0
+plotly.express==0.4.1
+altair==5.2.0
+
+To launch this project on a local machine, the data set should be accessible in the project folder along with the app.py file, and python lanched with the above listed packages installed. The app.py file contains the code to launch locally or to use with a Render account. From Terminal/command prompt when in the folder where the data set and app.py is listed, use: streamlit run app.py. Once launched, the URL to view the app locally will be listed in the Terminal/command prompt.
--- app.py
+++ app.py
@@ -4,11 +4,37 @@ import plotly.express as px
 
 veh_df = pd.read_csv('vehicles_us.csv')
 
-#full path '~/Documents/Data Science/Projects/TT_Sprint_4/TT_Sprint4_vehicles/vehicles_us.csv'
+st.header(':green[Vehicle advertisements and sales]')
 
-print(veh_df.head())
-
+#creating histograms with mileage, condition, type and year
 st.header('Histogram of mileage vs condition')
-
 fig_d = px.histogram(veh_df, x='odometer', color='condition', nbins=50)
 st.write(fig_d)
+
+st.header('Histogram of type vs model year')
+fig_a = px.histogram(veh_df, x='type', color='model_year')
+st.write(fig_a)
+
+#creating histogram with a checkbox to filter for vehicles priced over $10K
+st.header('Histogram of days listed vs condition')
+mask_filter = veh_df['price'] > 12000
+df_filtered = veh_df[mask_filter]
+highprice = st.checkbox("Priced > $12,000 (avg selling price)")
+if highprice:
+    fig_e = px.histogram(df_filtered, x='days_listed', color='condition')
+else:
+    fig_e = px.histogram(veh_df, x='days_listed', color='condition')
+st.write(fig_e)
+
+#creating additional histograms and scatterplots for price, color, mileage and days listed
+st.header('Histogram of price vs color')
+fig_b = px.histogram(veh_df, x='price', color='paint_color', nbins=100)
+st.write(fig_b)
+
+st.header('Plot of mileage vs price')
+fig_j = px.scatter(veh_df, x='odometer', y='price')
+st.write(fig_j)
+
+st.header('Plot of price vs days listed')
+fig_f = px.scatter(veh_df, x='price', y='days_listed', color='condition')
+st.write(fig_f)
